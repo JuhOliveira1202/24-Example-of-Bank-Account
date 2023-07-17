ceb9c229adf67fb6c1e456b1f513922ebbd2b4ef
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 14:07:06 +0100

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício
+{
+    internal class Conta
+    {
+        //ATRIBUTOS
+
+        private string titular;
+        private double saldo;
+        private int estado;
+
+        //CONSTRUTORES
+
+        public Conta() //C. VAZIO
+        {
+            titular = " Juliana Oliveira ";
+            saldo = 100;
+            estado = 1;
+        }
+
+        public Conta(string t, double s, int e) //C. PARÂMETROS
+        {
+            titular = t;
+            saldo = s;
+            estado = e;
+        }
+
+        public Conta(Conta c) //C. CÓPIA 
+        {
+            titular = c.getTitular();
+            saldo = c.getSaldo();
+            estado = c.getEstado();
+        }
+
+        //SELETORES
+
+        public string getTitular()
+        {
+            return titular;
+        }
+
+        public double getSaldo()
+        {
+            return saldo;
+        }
+
+        public int getEstado()
+        {
+            return estado;
+        }
+
+        public string getEstadoExt()
+        {
+            if (estado == 1)
+            {
+                return "Conta ativa";
+            }
+            else if (estado == 2)
+            {
+                return "Conta fechada";
+            }
+            else
+            {
+                return "Conta bloqueada";
+            }
+        }
+
+
+
+        //MODIFICADORES
+
+        public void setTitular(string t)
+        {
+            if(t != "" && t != null)
+            {
+                titular = t;
+            }
+        }
+
+        public void setEstado(int e)
+        {
+            if(e >= 1 && e <= 3)
+            {
+                estado = e;
+            }
+
+        }
+
+        public bool depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                saldo += valor;
+                return true;
+            }
+            return false;
+        }
+
+        public bool levantar(double valor)
+        {
+            if(saldo >= valor && estado == 1)
+            {
+                saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+
+        public bool transf(double v, Conta c) 
+        {
+            if (levantar(v) && c.getEstado() == 1)
+            {
+                c.depositar(v);
+                return true;
+            }
+            return false;
+        }
+
+        //COMPORTAMENTO
+
+        public override string ToString()
+        {
+            return "Titular: " + titular + "\n" + "Saldo: " + saldo + "\n" + "Estado: " + getEstadoExt();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Exercício;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //AULA 4 - CONTA
+        Console.WriteLine("----- Conta Corrente -----");
+
+        Conta c1 = new Conta();
+        //Console.WriteLine(c1.ToString());
+
+        c1.setTitular("Ana");
+        c1.depositar(50);
+
+        if (c1.depositar(-50))
+        {
+            Console.WriteLine("Ok");
+        }
+        else
+        {
+            Console.WriteLine("Not Ok");
+        }
+        Console.WriteLine(c1.ToString());
+
+        Conta c2 = new Conta();
+        c2.setTitular("Rui");
+        c1.transf(10,c2);
+        Console.WriteLine(c1.ToString());
+        Console.WriteLine(c2.ToString());
+    }
+}
